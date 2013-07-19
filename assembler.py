059700a45beba91e7944250e9bca42562def97b7
Alex Jose <ajose@caltech.edu>
2013-07-19 16:58:48 -0700

    change some names

---
+++ assembler.py
@@ -0,0 +1,9 @@
+
+"""
+what to do, what to do...
+
+tag-based?
+remember that this should be a base class for group 'definitions', not
+actually groups....change name to suit?
+Possible names: group_def, assembler, set_def, set_maker, master, definition
+"""
