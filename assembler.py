ede07cfbf59fd6e3e7647b022884fb6c5a508513
Alex B. Jose <ajose@caltech.edu>
2013-07-24 01:33:20 -0700

    minor changes

--- assembler.py
+++ assembler.py
@@ -4,7 +4,22 @@ import networkx as nx
 
 class Assembler(UnitGraph):
     
-    def __init__(self):
+    def __init__(self, simulator, tags=set()):
         super(Assembler, self).__init__()
+        self.S    = simulator # simulator to insert instances in to
+        self.tags = tags
+        
 
 
+    def set_graph(self, G):
+        # add own tags for every node in graph
+        for n in G:
+            G.node[n]['unit'].tags |= self.tags
+    
+    def copy_graph(self):
+        """ Make a deep copy of the graph. """
+        # Are you sure this is enough of a copy?!
+        # need to differentiate...so insert tags at copy-time?
+        # can differentiate just by calling get_uid!
+        return self.G.copy()
+# also perhaps allow for adding special/specific names when inserting instances...
--- simulator.py
+++ simulator.py
@@ -51,4 +51,3 @@ if __name__ == '__main__':
     #S.refresh_tags()
     #print S[1].__class__.__name__
 
-    S.copy_graph()
