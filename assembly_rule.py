050855b218bb71f3628857488001ec3c68b4ca35
Alex Jose <ajose@caltech.edu>
2013-07-25 16:02:44 -0700

    Initial version of assembly_rule

---
+++ assembly_rule.py
@@ -0,0 +1,26 @@
+
+
+class AssemblyRule:
+
+    def __init__(self, simulator, assembler, ):
+        # allow objects aside from assemblers?
+        self.S = simulator
+        self.A = assembler
+        self.tags  = set()
+        self.ports = {}
+
+
+
+    def update(self):
+        """ Update values with which Assembler objects will be initialized """
+        # raise nonimplemented error
+        pass
+
+    def make_population(self, pop_name, N):
+        """ Instantiate N versions of A """
+        for uid in range(N):
+            # update init things
+            self.update()
+            # insert instance
+            self.A.make_instance(tags=self.tags | {pop_name, pop_name+str(uid)},
+                                 ports=self.ports)
