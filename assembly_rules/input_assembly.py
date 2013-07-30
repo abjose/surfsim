949a1e8b3c8a964ab703393617d28bf4848a2d97
Alex Jose <ajose@caltech.edu>
2013-07-29 18:26:32 -0700

    Initial commit of input_assembly

---
+++ assembly_rules/input_assembly.py
@@ -0,0 +1,13 @@
+
+from assembly_rule import AssemblyRule
+
+
+class InputAssembly(AssemblyRule):
+    
+    def __init__(self, ):
+        super(InputAssembly, self).__init__(...)
+
+
+
+if __name__ == '__main__':
+    pass
