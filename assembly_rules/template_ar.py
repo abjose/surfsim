9d8e84ac59457d770cbfdec42a24c3e438476d96
Alex B. Jose <ajose@caltech.edu>
2013-07-30 23:29:34 -0700

    add a template for assembly rules

---
+++ assembly_rules/template_ar.py
@@ -0,0 +1,13 @@
+
+from assembly_rule import AssemblyRule
+
+class TemplateAR(AssemblyRule):
+
+    def __init__(self, simulator, assembler):
+        super(TemplateAR, self).__init__(simulator, assembler)
+
+    def step(self):
+        """ Update values with which Assembler objects will be initialized """
+        raise NotImplementedError()
+        # TODO: consider using lazyarrays
+        pass
