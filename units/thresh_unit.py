5606cc7fe38b0efd84e27a0abe44f606c2f6bdd7
Alex Jose <ajose@caltech.edu>
2013-08-05 16:45:50 -0700

    add thresh_unit

---
+++ units/thresh_unit.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+
+from unit import Unit
+
+
+class ThreshUnit(Unit):
+    # should probably do sigmoid
+
+    def __init__(self, simulator, uid=None, tags=set(), ports={}):
+        super(ThreshUnit, self).__init__(simulator, thresh, c, r0, rmax,  
+                                         uid=uid, tags=tags, ports=ports)
+        self.set_port('input', 0)
+        self.set_port('output', 0)
+
+    def operation(self):
+        # set rate to signal * c if signal > thresh, else r0.
+        # could just do most recent...
+        signal = np.array(self.ports['input'])
+        signal[signal < thresh]  = r0
+        signal[signal >= thresh] *= c
+        self.set_port('output', signal)
+        
+
+if __name__ == '__main__':
+    pass
