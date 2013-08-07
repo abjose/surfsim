1c9ce44b5e867fc1077e2a332d19e22ae767e9ce
Alex B. Jose <ajose@caltech.edu>
2013-08-07 12:08:18 -0700

    remove print

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -5,7 +5,6 @@ import os, sys
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-print 'connector stuff'
 for py in [f[:-3] for f in os.listdir(path) if f.endswith('.py') and f != '__init__.py']:
     mod = __import__('.'.join([__name__, py]), fromlist=[py])
     classes = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
