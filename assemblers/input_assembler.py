4819441457b13ba229f947d71c3b80151e28e9ca
Alex B. Jose <ajose@caltech.edu>
2013-08-07 12:06:57 -0700

    try to fix some tagging issues

--- assemblers/input_assembler.py
+++ assemblers/input_assembler.py
@@ -1,10 +1,16 @@
 
+from units import InputUnit
 from assembler import Assembler
 # need to do something like surfsim.units import InputUnit?
-from units import InputUnit
 
 class InputAssembler(Assembler):
 
     def __init__(self, simulator, AR, tags=set(), ports={}):
+        print 'input-tags', tags
+        print 'input-ports', ports
+        ports.update(dict(input_src=AR))
         super(InputAssembler, self).__init__(simulator, tags=tags, ports=ports)
-        self.add_unit(unit=InputUnit, ports=dict(input_src=AR))
+        #ports.update(dict(input_src=AR))
+        self.add_unit(unit  = InputUnit, 
+                      tags  = tags | {'input_unit'}, 
+                      ports = ports)
